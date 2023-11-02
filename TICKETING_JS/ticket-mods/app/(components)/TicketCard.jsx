4a678ed0f5771d425c3b53c3b3ce224af3b249e2
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-11-02 21:08:22 +0100

    TICKET APP 14/25 🎆🎇 mvp?

--- TICKETING_JS/ticket-mods/app/(components)/TicketCard.jsx
+++ TICKETING_JS/ticket-mods/app/(components)/TicketCard.jsx
@@ -7,11 +7,26 @@ import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-        <DeleteBlock />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
         <PriorityDisplay />
-        <ProgressDisplay />
-        <StatusDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">this is the ticket description! Please solve this ticket</p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">11/02/23 21:15</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   )
 }
