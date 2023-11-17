5144aad9608cbc778eec3e3e192fb1bac87a00ba
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-11-17 20:15:18 +0100

    40% JS BASIC DATA_ STRUCTURE  🎇🎭

--- JS_ALG_DATASTRUC/BASIC_DATASTRUCTURES_js.js
+++ JS_ALG_DATASTRUC/BASIC_DATASTRUCTURES_js.js
@@ -55,3 +55,68 @@ console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
 
 /* Remove Items Using splice() */
+// splice() can take up to 3 arguemnts, whereas first 2 represents indexes+ positions.
+//examples:
+let array=['today', 'was', 'not', 'so', 'great'];
+array.splice(2,2);	// +> ['today', 'was', 'great'].
+//or||
+let array = ['I', 'am', 'feeling', 'really', 'happy'];
+let newArray = array.splice(3, 2);
+//exercise:
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(0,1);
+arr.splice(3,6);
+
+console.log(arr);	// [4,5,1] : score=10.
+
+
+/* Add Items Using splice() */
+// sample:
+const numbers=[10,11,12,12,15];
+const startIndex=3;
+const amountToDelete=1;
+numbers.splice(startIndex, amountToDelete,13,14);
+console.log(numbers);	// +> [10,11,12,13,14,15];
+
+function htmlColorNames(arr) {
+  arr.splice(0,2,'DarkSalmon','BlanchedAlmond');
+  return arr;
+}
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+/* Copy Array Items Using slice(); */
+let weatherConditions=['rain', 'snow', 'sleet', 'hail', 'clear'];
+let todaysWeather=weatherConditions.slice(1,3);		// +> todaysWeather==['snow','sleet'].
+// and weatherConditions remain the same.
+
+function forecast(arr) {
+  let weather=arr.slice(2,4);		// it doesn't work on arr object only from parameter.
+  return weather;
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
+/* Copy an Array with the Spread operator; */
+let thisArray=[true,true,udnefined,false,null];
+let thatArray=[...thisArray];		// +> thatArray ==  [true, true, undefined, false, null].
+
+//exercise:
+function copyMachine(arr, num) {
+	let newArr=[];
+	for( let i=0; i<num;  i++) {
+			newArr.push([...arr]);
+	}
+	return newArr;
+}
+//or: ||
+function copyMachine(arr,num) {
+	let newArr=[];
+	while(num>=1) {
+		newArr.push([...arr]);
+		num--;
+	}
+	return newArr;
+} console.log(copyMachine([true,false,true],2));		// +> [[true,false,true],[true,false,true]]
+
+
