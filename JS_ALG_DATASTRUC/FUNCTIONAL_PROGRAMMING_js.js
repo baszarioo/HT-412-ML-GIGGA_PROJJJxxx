457ac98da3e83d8dad734ed9cd98d572dc2f3eae
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-02 19:05:10 +0100

    [JS FUNCTIONAL Pr 25%] copying, altering 😋🧯

--- JS_ALG_DATASTRUC/FUNCTIONAL_PROGRAMMING_js.js
+++ JS_ALG_DATASTRUC/FUNCTIONAL_PROGRAMMING_js.js
@@ -48,3 +48,115 @@ console.log(
 	tea4GreenTeamFCC,
 	tea4BlackTeamFCC,
 );	// +> ['greenTea','greenTea' (x12) ] ['blackTea', 'blackTea' (x26) ]
+
+
+
+/* UNDERSTAND THE HAZARDS OF USING IMPERATIVE CODE  */
+// one example is to instead of the for loop, to call the map method which handles the details of iterating over an array.
+
+//version1: not really working.
+function Window(tabs) {
+	this.tabs=tabs;
+}
+Window.prototype.tabClose = function (index) {
+	var tabsBeforeIndex = this.tabs.slice(0, index);
+	var tabsAfterIndex = this.tabs.slice(index+1); 
+	this.tabs = tabsBeforeIndex.concat(tabsAfterIndex);
+	return this.tabs;
+};
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']);
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']);
+var videoWindow =new Window(['Netflix', 'Youtube', 'Vine']);
+ var finalTabs = join(
+	tabOpen.bind(workWindow), tabClose.bind(workWindow, 2),
+	tabOpen.bind(socialWindow),
+	tabClose.bind(socialWindow, 2), tabOpen.bind(socialWindow),
+	tabOpen.bind(videoWindow),
+	tabClose.bind(videoWindow, 1),
+	join.bind(null)
+);
+console.log(finalTabs.tabs);//no no 
+
+//solution1.  change 1 into 2.
+const tabsAfterIndex = this.tabs.splice(index + 1); //+>
+const tabsAfterIndex = this.tabs.splice(1);
+
+//or solution2:  change lines.1 into lines.2.
+const tabsBeforeIndex = this.tabs.splice(0, index);
+const tabsAfterIndex= this.tabs.splice(index + 1);
+// +> replace with <+ :
+const tabsBeforeIndex = this.tabs.slice(0, index);
+const tabsAfterIndex= this.tabs.slice(index + 1);
+
+
+//actual valid code:
+const Window = function(tabs) {
+	this.tabs = tabs;
+};
+Window.prototype.join = function(otherWindow) {
+	this.tabs=this.tabs.concat(otherWindow.tabs);
+	return this;
+};
+Window.prototype.tabOpen= function(tab) {
+	this.tabs.push('new tab');
+	return this;
+};
+
+Window.prototype.tabClose= function(index){
+	const tabsBeforeIndex=this.tabs.splice(0,index);
+	const tabsAfterIndex = this.tabs.splice(1);
+	this.tabs=tabsBeforeIndex.concat(tabsAfterIndex);
+	return this;
+};
+
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']);
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']);
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']);
+const finalTabs = socialWindow
+	.tabOpen()
+	.join(videoWindow.tabclose(2))
+	.join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+
+
+
+/* AVOID MUTATIONS AND SIODE EFFECTS USING FUNCTIONAL PROGRAMMING */
+// In functional programming, changing or altering things is called mutation, and the outcome we call a: side effect. A function, ideally should be a pure function, meaning that it does not cause any side effects.
+let fixedValue=4;
+function incrementer() {
+	let val=fixedValue;
+	return ++val;
+}
+
+
+
+/* PASS ARGUMENTS TO AVOID EXTERNAL DEPENDENCE IN A FUNCTION */
+// another principle of functional programming is to always declare your dependencies explicitly. i.e. pass the variable or object we want to use - directly into the function as an argument.
+let fixedValue = 4;
+function incrementer(fixedValue) {
+	let val=fixedValue;
+	return ++val;
+}
+
+
+
+/* REFACTOR GLOBAL VARIABLES OUT OF FUNCTIONS */
+//altering object or variable by = const newArr = arrVar, is not copying, but creating reference!!@!
+//let newArr=[...arr];
+//function remove(list,bookName) { return list.filter(book => book !== bookName); }
+//function add(list, bookName) { return [...list, bookName]; }
+
+const bookList=["The hound of the Baskervilles", "On the Electrodynamics of Moving Bodies", "Philosophia Naturalis Principia Mathematica", "Disquisitions Arithmeticae"];
+function add(arr, bookName) [
+	let bList = [...arr];
+	bList.push(bookName);
+	return bList;
+}
+function remove(arr,bookName){
+	let bList=[...arr];
+	const book_index = bList.indexOf(bookName);
+	if(book_index >= 0) {
+		bList.splice(book_index, 1);
+		return bList;
+	}
+}
