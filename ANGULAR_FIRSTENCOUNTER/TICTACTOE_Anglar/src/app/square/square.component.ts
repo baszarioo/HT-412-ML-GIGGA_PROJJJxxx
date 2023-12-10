7ab7f4cf1aafdad0e41345932cdb01eb2ac45dbe
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-10 23:25:07 +0100

    [ANGULAR nx] TICTACTOE part 1 - build nx>ng 🧓🏾🎠
    USE NX in 2023

---
+++ ANGULAR_FIRSTENCOUNTER/TICTACTOE_Anglar/src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  // styleUrl: './square.component.scss'
+
+})
+export class SquareComponent {
+
+}
