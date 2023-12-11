cedeae197229dfc5e1d9affa6d55352675c4e395
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-11 20:18:52 +0100

    [JS_ALG_SCRIPTS]: to finish 5th implement. parsin 😍🥠

--- JS_ALG_DATASTRUC/INTERM_ALG_SCRIPT_js.js
+++ JS_ALG_DATASTRUC/INTERM_ALG_SCRIPT_js.js
@@ -144,11 +144,169 @@ function whatIsName(collection, source){
 function whatNameIs(collection, source) {
 	const sourceKeys=Object.keys(source);
 	return collection.filter(obj=> {
-		for(lety i=0; i<sourceKeys.length; i++){ 
+		for(let i=0; i<sourceKeys.length; i++){ 
 			if(obj[sourceKeys[i]] !== source[sourceKeys[i]]){
 				return false;
 			}
 		}
 		return true;
 	});
+//20%
+
+
+
+
+/* // SPINAL TAP CASE // */
+// v 1:
+function spinalCase(str) {
+	var regex=/\s+|_+/g;	//variable for white spaces and underscores.
+	str = str.replace(/([a-z])([A-Z])/g, "$1 $2");	 // replace low-upper case to low-space-uppercase
+	return str.replace(regex, "-").toLowerCase();	// replace space and _ with -
 }
+// v 2;
+function spinalCase(str) {
+	str=str.replace(/([a-z])([A-Z])/g, "$1 $2");
+	return str
+		.toLowerCase()
+		.split(/(?:_| )+/)
+		.join("-");
+}
+// v 3:
+function spinalCase(str) {
+	return str
+		.split(/\s|_|(?=[A-Z])/)
+		.join("-")
+		.toLowerCase();
+}
+
+
+
+/* // PIG LATIN // */
+// add ay or way to the word.
+function translatePigLatin(str){
+	let consonantRegex=/^[^aeiou]+/;
+	let myConsonants = str.match(consonantRegex);
+	return myConsonants !== null
+		? str
+			.replace(consonantRegex, "")
+			.concat(myConsonants)
+			.concat("ay")
+		: str.concat("way");
+}
+// v 2;
+fucntion translatePigLatin2(str){
+	var pigLatin="";
+	var regex = /[aeiou]/gi;
+	if(str[0].match(regex)) {
+		pigLatin = str+"way";
+	} else if (str.match(regex) === null) {
+		pigLatin = str + "ay";
+	} else {
+		var vowelIndice = str.indexOf(str.match(regex)[0]);	// find how many consonants before the 1st vowel.
+		pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+	}
+	return pigLatin;
+}
+// v 3 ;
+function translatePigLatin3(str){
+	if(str.match(/^[aeiou]/)) return str+"way";
+	const consonantCluster = str.match(/^[^aeiou]+/)[0];
+	return str.substring(consonantCluster.length) + consonantCluster + "ay";
+}
+// v 4;
+function translatePigLatin4(str) {
+	return str
+		.replace(/^[aeiou]\w*/, "$&way")
+		.replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+}
+// v 5; iter;
+function translatePigLatin5(str, charPos=0) {
+	return ['a','e','i','o','u'].includes(str[0])
+		?str + (charPos === 0 ? 'way' : 'ay')
+		: charPos === str.length
+			? str + 'ay'
+			:translatePigLatin(str.slice(1) + str[0], charPos + 1);
+}
+
+
+
+/* // SEARCH AND REPLACE // */
+// V1: 
+function myReplace1(str, before, after) {
+	var index=str.indexOf(before);
+	if(str[index]=== str[index].toUpperCase()) {
+		after=after.charAt(0).toUpperCase() + after.slice(1);
+	} else {
+		after = after.charAt(0).toLowerCase() + after.slice(1);
+	}
+	str = str.replace(before, after);
+	return str;
+}
+// V2;
+function myReplace(str,before,after) {
+	if(/^[A-Z]/.test(before)) {
+		after=after[0].toUpperCase() + after.substring(1);
+	} else {
+		after=after[0].toLowerCase() + after.substring(1);
+	}
+	return str.replcae(before, after);
+}
+//V3:
+function myReplcae(str, before, after) {
+	function applyCasing(source,target) {
+		var targetArr=target.split("");
+		var sourceArr=target.split("");
+		for(var i=0; i<Math.min(targetArr.length, sourceArr.length); i++) {
+			if(/[A-Z]/.test(sourceArr[i])) {
+				targetArr[i] = targetArr[i].toUpperCase();
+			}
+			else targetArr[i] = targetArr[i].toLowerCase();
+		}
+		return targetArr.join("");
+	}
+	return str.replace(before, applyCasing(before, after));
+}
+// V $4;
+//method to the string object, not overriding the existing one (if it is).
+String.prototype.capitalize = 
+	String.prototype.capitalize || 
+	function() {
+		return this[0].toUpperCase() + this.slice(1);
+	};
+const Util=(function() {
+	function textCase(str, tCase) {
+		if(tCase) {
+			return setCase(str, tCase);
+		} else {
+			return getCase(str);
+		}
+		function setCase(str, tCase) {
+			switch(tCase) {
+				case "uppercase":
+					return str.toUpperCase();
+				case "lowercase":
+					return str.toLowerCase();
+				case "capitalized":
+					retrun str.capitalize();
+				default:
+					return str;
+			}
+		}
+		function getCase(str) {
+			if(str===str.toUpperCase()) {
+				return "uppercase";
+			}
+			if(str===str.toLowerCase()) {
+				return "lowercase";
+			}
+			if(str===str.capitalize()) {
+				return "capitalized";
+			}
+			return "normal";
+		}
+	}
+	return {
+		textCase
+	};
+})();
+	}
