6b8385edfd23e46d418120fcc5d67d68ef021b9f
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-13 12:59:13 +0100

    CREATE 4_REACT_FASTTATTACK boi 🎐🌶
    🎎🎎

---
+++ FRONT_END_LIBRARIES/4_REACT_FASTATTACK.jsx
@@ -0,0 +1,4 @@
+/* React = js lib, for building reusable, component-driven user interfaces for webpages and apps.
+combines HTML and JS functionaltiy into JSX.	Easy managing the flow of data throughout the app..
+LEARN: coimponents, managing data in the form of state proops, use different lifecycle methods like :
+componentDidMount, etc.	*/
