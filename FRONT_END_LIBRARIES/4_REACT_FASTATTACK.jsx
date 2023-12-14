aaf566839a09e81616c0863e64ce55e2c1b59af7
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-15 00:55:58 +0100

    [REACT course;(1)]: quick session ~36% 👸🏽🕋

--- FRONT_END_LIBRARIES/4_REACT_FASTATTACK.jsx
+++ FRONT_END_LIBRARIES/4_REACT_FASTATTACK.jsx
@@ -237,3 +237,127 @@ class TypesOfFood extends React.Component {
 	}
 };
 ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
+
+
+
+
+
+/* /2//	Write a React Component from Scratch;. //2/ */
+//class that extends React.Component has a null-return, [redefined render method.
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<h1>My First React Component!</h1>
+			</div>
+		);
+	}
+};
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+
+
+/* /2//	Pass props to a Stateless Functional Component.:;. //2/ */
+//this is how u would add an user-property to welcome method.
+<App>
+	<Welcome user='Mark' />
+</App>
+//and here's how you would define a component(stateless functional one).
+const Welocme = (props) => <h1>Hello, {props.user}!</h1>
+
+//exercise:
+// <p>The current date is: {props.date}</p>
+// <CurrentDate date={Date()} />
+const CurrentDate = (props) => {
+	return (
+		<div>
+			<p>The current date is: {props.date}</p>
+		</div>
+	);
+};
+class Calendar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<h3>What date is it?</h3>
+				<CurrentDate date={Date()} />
+			</div>
+		);
+	}
+};
+
+
+/* /2//	Pass an Array as Props. //2/ */
+//example:
+<ParentComponent>
+	<ChildComponent colors={["green", "blue", "red"]} />
+</ParentComponent>
+//child comp. then has access to the array property = colors. - can use array methods:
+const ChildComponent = (props) => <p>{props.colors.join(', ')}</p>
+//exx
+const List = (props) => {
+	return <p>{props.tasks.join(', ')}</p>
+//	return <p>{}</p>
+};
+class ToDo extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<h1>To Do Lists</h1>
+				<h2>Today</h2>
+				<List tasks={["Walk", "Cook", "Bake"]} />
+				<h2>Tomorrow</h2>
+				<List tasks={["Study", "Code", "Eat"]} />
+			</div>
+		);
+	}
+};
+
+
+
+/* /2//	Use Default Props. //2/ */
+// you can specify default prop as: 
+MyComponent.defaultProps = { location: 'San Francisco' };
+// but if you provide 'null' as the value for prop, it stays null; //if empty => San Francisco;
+const ShoppingCart = (props) => {
+	return (
+		<div>
+			<h1>Shopping Cart Component </h1>
+		</div>
+	)
+};
+ShoppingCart.defaultProps = { items: 0 };
+
+
+
+/* /2//	Override Default Props. //2/ */
+const Items = (props) => {
+	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+Items.defaultProps = {
+	quantity: 0
+}
+class ShoppingCart extends React.Component {
+	constructor(props0 {
+		super(props);
+	}
+	render() {
+		return <Items quantity={10}/>
+	}
+};
+
+
+
+/* /2//Use Prototypes to Define the Props you Expect.. //2/ */
+/* react provides type-checking features (example: API call to retrieve data you expect in an array).
+ it's considered a best practice to set 'propTypes' when you know type of a prop ahead of time. 
+  example: require the type: function for a prop: handleClick */
+ MyComponent.propTypes = {handleClick: PropTypes.func.isRequired };		// x...x
