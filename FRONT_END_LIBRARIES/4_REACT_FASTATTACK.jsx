5ea05c9fa5e70e4b6927ce39ab257c1a75eaf86d
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-15 16:06:47 +0100

    [REACT q_session]50%; render, bind, setState, this. 🦂👽

--- FRONT_END_LIBRARIES/4_REACT_FASTATTACK.jsx
+++ FRONT_END_LIBRARIES/4_REACT_FASTATTACK.jsx
@@ -346,7 +346,7 @@ Items.defaultProps = {
 	quantity: 0
 }
 class ShoppingCart extends React.Component {
-	constructor(props0 {
+	constructor(props) {
 		super(props);
 	}
 	render() {
@@ -361,3 +361,245 @@ class ShoppingCart extends React.Component {
  it's considered a best practice to set 'propTypes' when you know type of a prop ahead of time. 
   example: require the type: function for a prop: handleClick */
  MyComponent.propTypes = {handleClick: PropTypes.func.isRequired };		// x...x
+// as of v15.5.0, PropTypes is imported independently from React, like this: 
+import PropTypes from 'prop-types';
+///////////!! inside Items.<method> syntax we do not put semicolons(;) at the end of line.
+const Items = (props) => {
+	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+Items.propTypes = {
+	quantity: PropTypes.number.isRequired
+};
+Items.defaultProps = {
+	quantity: 0
+};
+class ShoppingCart extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <Items />
+	}
+};
+
+
+
+/* /2// Access Props Using this.props; //2/ */
+// use a : {this.props.data}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<Welcome name={"Kurt"}/>
+			</div>
+		);
+	}
+};
+class Welcome extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<p>Hello, <strong>{this.props.name}</strong>!</p>
+			</div>
+		);
+	}
+};
+
+
+/* /2.5// Review Using Props with Stateless functional components; //2.5/ */
+// a stateless functional component is any function we write which accepts props and returns JSX.
+//example//
+class CampSite extends React.Component {
+	constructor(props){
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<Camper />
+			</div>
+		);
+	}
+};
+const Camper = props => <p>{props.name}</p>;
+Camper.defaultProps = {
+	name: "CamperBot"
+};
+Camper.propTypes = {
+	name: PropTypes.string.isRequired
+};
+
+
+
+/* //2.5// Create a Stateful Component; //2.5// */
+// 1 of the most importatnt topics for Reacts is STATE. it has to be set to a JavaScript object.
+//exercise:  Initialize the component with state in the constructor and assign your name to a property of firstname.
+class StatefulComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "Name"
+		}
+	}
+	render() {
+		return (
+			<div><h1>{this.state.name}</h1></div>
+		);
+	}
+};
+// or "|"
+class StatefulComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: "Name",
+			surrname: "Stone"
+		}
+	}
+	render() {
+		return (
+			<div>
+				<h1>{this.state.firstName}</h1>
+				<h2>{this.state.surrname}</h2>
+			</div>
+		);
+	}
+};
+
+
+
+/* //2.5// ;Render State in The User Interface; //2.5// */
+class MyComponent extends React.component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'freeCodeCamp'
+		}
+	}
+	render() {
+		return (
+			<div>
+				<h1>{this.state.name}</h1>
+			</div>
+		);
+	}
+};
+
+
+
+/* //2.5// ;Render State in The User Interface ANOTHER WAY!; //2.5// */
+//you can declare functions, or access data from state or pros, before the return statement.
+//Exercise:render this value in an h1 tag using the variable name. Remember, you need to use the JSX syntax (curly braces for JavaScript) in the return statement.
+class MyComponent extends React.Component { 
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'freeCodeCamp'
+		}
+	}
+	render() {
+		const name = this.state.name;
+		return (
+			<div>
+				<h1>{name}</h1>
+			</div>
+		);
+	}
+};
+
+
+
+/* //2.5// ;Set State with this.setState; //2.5// */
+//example1: update username in state:	// call setState method within your component class.
+this.setState({
+	username: 'Lewis'
+});
+// updates through the setState method can be asynchronous. (it's action doesnt has to happen immediately).
+// value on screen updates, but in console it's delayed//or doesn't happen at all..[shows old value].
+
+//asynchronous standard-1:
+class Conuter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			counter: 0
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick() {
+		this.setState((prevState) => {
+			return {
+				counter: prevState.counter + 1
+			};
+		});
+		console.log("counter", this.state.counter);
+	}
+	render() {
+		const { counter } = this.state;
+		return (
+			<div>
+				<button onClick={this.handleClick}>Increment counter</button>
+				<div>Counter value is {counter}</div>
+			</div>
+		);
+	}
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Counter />, rootElement);
+// improved counter (synchronized).
+import React from "react";
+import ReactDOM from "react-dom";
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state= {
+			counter: 0
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick() {
+		this.setState(
+			(prevState) => {
+				return {
+					counter: prevState.counter + 1
+				};
+			}, //!!!!
+			() => console.log("counter", this.state.counter)
+		);
+	}
+	// render() { ... }
+}
+const rootElement= document.getElementById("root");
+ReactDOM.render(<Counter />, rootElement);
+//then in index.html u access this by creating just: <div id="root"></div>
+// works as for react: "17.0.2"
+		
+//back to exercise: update the component state, after clicking button>
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state={
+			name: "Initial State"
+		};
+		this.handleClick=this.handleClick.bind(this);
+	}
+	handleClick() {
+		this.setState({
+			name: 'React Rocks!'
+		});
+	}
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleClick}>Click Me</button>
+				<h1>{this.state.name}</h1>
+			</div>
+		);
+	}
+};		//;part1'
