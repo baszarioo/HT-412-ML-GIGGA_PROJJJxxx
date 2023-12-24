eca559c170281e0da3279a2263a3a5ef570f53ac
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-24 19:32:24 +0100

    [ANGULAR SREDDIT]: signup.validation 🎊🏷

--- ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/auth/signup/signup.component.ts
+++ ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SignupRequestPayload } from './signup-request.payload';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +11,7 @@ import { SignupRequestPayload } from './signup-request.payload';
 export class SignupComponent implements OnInit {
   signupRequestPayload: SignupRequestPayload;
   signupForm: FormGroup;
-  constructor() {
+  constructor(private authService: AuthService) {
     this.signupRequestPayload = {
       username: '',
       email: '',
@@ -29,5 +30,10 @@ export class SignupComponent implements OnInit {
     this.signupRequestPayload.email = this.signupForm.get('email').value;
     this.signupRequestPayload.username = this.signupForm.get('username').value;
     this.signupRequestPayload.password = this.signupForm.get('password').value;
+    // authService from signupReq. 
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 }
