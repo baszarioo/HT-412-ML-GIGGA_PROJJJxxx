a12dc590ca35d0188d94a9cbe001640f2dfd00ef
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-26 00:23:43 +0100

    [ANG SRD]: tokens improv 🎊🏷

--- ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/auth/shared/auth.service.ts
+++ ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/auth/shared/auth.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SignupRequestPayload } from '../signup/signup-request.payload';
-import { Observable  } from 'rxjs';
+import { Observable, throwError  } from 'rxjs';
 import { LoginRequestPayload } from '../login/login-request.payload';
 import { LocalStorageService } from 'ngx-webstorage/public_api';
-import { map } from 'rxjs/operators';
+import { map, tap, catchError } from 'rxjs/operators';
 import { LoginResponse } from '../login/login-response.payload';
 @Injectable({
   providedIn: 'root'
@@ -28,4 +28,73 @@ export class AuthService {
         return true;
       }));
   }  
+  getJwtToken() {
+    return this.localStorage.retrieve('authenticationToken');
+  }
+  refreshToken(): Observable<LoginResponse> {
+    const refreshTokenPayload = {
+      refreshToken: this.getRefreshToken(),
+      username: this.getUsername()
+    };
+    console.log(refreshTokenPayload)
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', refreshTokenPayload)
+      .pipe(
+        tap(response => {
+          this.storeAuthData(response);
+          console.log(`response: + ${response}`)
+        }),
+        catchError((error) => {
+          console.error('Error during token refresh:', error);
+          return throwError(error);
+        })
+      );
+  };
+  getUsername(): string | null {
+    return this.localStorage.retrieve('username');
+  }
+  getRefreshToken(): string | null {
+    return this.localStorage.retrieve('refreshToken');
+  }
+  checkExpiry(): boolean {
+    const expirationDate = this.localStorage.retrieve('expiresAt');
+    if(!expirationDate) {
+      return true;
+    }
+    const now=new Date().getTime() / 1000;
+    const expirationTime = parseFloat(expirationDate);
+    const bufferTimeInSeconds = 1;
+    return expirationTime - bufferTimeInSeconds <= now;
+  }
+  logout() {
+    const refreshToken = this.getRefreshToken();
+    const username = this.getUsername();
+     if(refreshToken && username) {
+      this.httpClient.post('http://localhost:8080/api/auth/logout', {refreshToken, username },
+      {responseType: 'text' })
+        .subscribe({
+          next: data => {
+            console.log(data);
+          },
+          error(err){
+            console.error('Error during logout:', err);
+            throw err;
+          },
+        });
+     }
+    //  to move into substructure
+     this.localStorage.clear('authenticationToken');
+     this.localStorage.clear('username');
+     this.localStorage.clear('refreshToken');
+     this.localStorage.clear('expiresAt');
+  }
+  private storeAuthData(data: LoginResponse){
+    this.localStorage.clear('authenticationToken');
+    this.localStorage.clear('expiresAt');
+    this.localStorage.clear('refreshToken');
+    this.localStorage.clear('username');
+    this.localStorage.store('authenticationToken', data.authenticationToken);
+    this.localStorage.store('refreshToken', data.refreshToken);
+    this.localStorage.store('expiresAt', data.expiresAt);
+    this.localStorage.store('username', data.username);
+  }
 }
