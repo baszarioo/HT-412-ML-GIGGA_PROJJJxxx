c753a2b0b3f993f2ec922b0d074e942f8d8e8495
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-25 22:26:17 +0100

    [ANG SRD]: login signup tokens part2. 🎊🏷

--- ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/auth/login/login.component.ts
+++ ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/auth/login/login.component.ts
@@ -13,6 +13,8 @@ import { ToastrService } from 'ngx-toastr/public_api';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loginRequestPayload: LoginRequestPayload;
+  isError: boolean = false;
+  registerSuccessMessage: string;
   constructor(private authService: AuthService, private activatedRoute: ActivatedRoute,
     private router: Router, private toastr: ToastrService ) {
     this.loginRequestPayload = {
@@ -32,13 +34,22 @@ export class LoginComponent implements OnInit {
         this.toastr.warning('Please Check you inbox for activation email ' 
           + 'activate your account before you Login!');
       }
-     })
+     });
   }
   login() {
     this.loginRequestPayload.username = this.loginForm.get('username').value;
     this.loginRequestPayload.password = this.loginForm.get('password').value;
-    this.authService.login(this.loginRequestPayload).subscribe(data => {
-      console.log('Login successful');
-    });
+    this.authService.login(this.loginRequestPayload)
+    .subscribe({
+      next: data => {
+        this.isError =false;
+        this.router.navigateByUrl('/');
+        this.toastr.success('Login Successful');
+      },
+      error: (err) => {
+        this.isError = true;
+        this.toastr.error(`${err.error.username}`);
+      },
+    })
   }
 };
