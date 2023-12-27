6085bc08b27b6fbb5b62f61761cf1ebc483751db
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-12-27 19:14:14 +0100

    [SREDD]: added view of spec. post 🏷👍

---
+++ ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/post/view-post/view-post.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostModel } from 'src/app/shared/post-model';
+import { PostService } from 'src/app/shared/post.service';
+
+@Component({
+  selector: 'app-view-post',
+  templateUrl: './view-post.component.html',
+  styleUrls: ['./view-post.component.css']
+})
+export class ViewPostComponent implements OnInit {
+  postId: number;
+  post: PostModel;
+  constructor(private postService: PostService, private activateRoute: ActivatedRoute) {
+    this.postId = this.activateRoute.snapshot.params['id'];
+    this.postService.getPost(this.postId).subscribe({
+      next: (data) => {
+        this.post = data;
+      }, error: (error) => {
+        throw error;
+      }
+    });
+  }
+  ngOnInit(): void {
+    
+  }
+}
--- ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/shared/post-tile/post-tile.component.ts
+++ ANGULAR_FIRSTENCOUNTER/sreddit_app/sreddit_angular/src/app/shared/post-tile/post-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { PostModel } from '../post-model';
 import { PostService } from '../post.service';
 import { Router } from '@angular/router'
@@ -8,7 +8,8 @@ import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 @Component({
   selector: 'app-post-tile',
   templateUrl: './post-tile.component.html',
-  styleUrls: ['./post-tile.component.css']
+  styleUrls: ['./post-tile.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PostTileComponent implements OnInit {
   public faArrowUp: IconDefinition = faArrowUp;
@@ -16,7 +17,11 @@ export class PostTileComponent implements OnInit {
   public faComments: IconDefinition = faComments;
   @Input() posts: PostModel[];
   constructor(private postService: PostService, private router: Router) {
-
+    this.postService.getAllPosts().subscribe({
+      next: (post) => {
+        this.posts = post;
+      }
+    });
   }
   ngOnInit(): void {
     
