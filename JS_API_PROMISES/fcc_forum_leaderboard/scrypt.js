45dda06ef593e30f400f032d97b650f7c745175c
baszarioo <113721856+baszarioo@users.noreply.github.com>
2024-01-31 22:23:53 +0100

    [fcc] forum_leaderboard - init

---
+++ JS_API_PROMISES/fcc_forum_leaderboard/scrypt.js
@@ -0,0 +1,21 @@
+const forumLatest = "https://forum-proxy.freecodecamp.rocks/latest";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer=document.getElementById('posts-container');
+
+const fetchData = async() => {
+	try {
+		const res = await fetch(forumLatest);
+		const data = await res.json();
+		console.log(data);
+		showLatestPosts(data);
+	} catch(err) {
+		console.log(err);
+	}
+};
+fetchData();
+
+const showLatestPosts = (data) => {};
