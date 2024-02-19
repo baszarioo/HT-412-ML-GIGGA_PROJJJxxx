fdd692fe33edb04d6812ed3cda18ff727bd69a09
baszarioo <113721856+baszarioo@users.noreply.github.com>
2024-02-19 17:18:18 +0100

    [fcc] mocca tests: send/put V2 🐦🐦 WIP

--- JS_QA_COURSE/chai_tests_course/mocca_unit_tests_2.js
+++ JS_QA_COURSE/chai_tests_course/mocca_unit_tests_2.js
@@ -88,4 +88,23 @@ To send a 'PUT' request and a JSON object to the '/travellers' endpoint, you can
 					done();
 				});
 		});
+		// -----------------------------------------------------------------------------
+		test('Send {surname: "da Verrazzano"}', function (done) {
+			chai
+				.request(server)
+				.put('/travellers')
+				.send({ surname: 'da Verrazzano' })
+				.end(function(err, res) {
+					assert.equal(res.status, 200, 'response status should be 200');
+					assert.equal(res.type, 'application/json', 'Response should be json');
+					assert.equal(res.body.name, 'Giovanni');
+					assert.equal(res.body.surname, 'da Verrazzano');
+					done();
+				});
+		 });
+		});
+	});
+	// -----------------------------------------------------------------------------
+	
+
 		
